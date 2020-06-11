26cdc373360578e3f8cdf11b440b5d7a08851e92
Lizbeth Moreno <lizbethmoreno989@gmail.com>
2020-06-11 17:51:24 -0500

    crea archivos iniciales del proyecto

---
+++ AJAX/app.js
@@ -0,0 +1,30 @@
+document.getElementById('cargar').addEventListener('click', cargarDatos);
+
+function cargarDatos(){
+    const xhr = new XMLHttpRequest();
+
+    //Abrir una conexión
+    xhr.open('GET', 'datos.txt', true);
+    
+    xhr.onreadystatechange = function(){
+
+        console.log('Estado ${this.readyState}');
+          
+        if(this.readyState === 4 && this.status === 200){
+        
+
+      }
+
+      //ready status
+      /*
+      0 - No iniciado
+      1.-Conexión establecida
+      2.- Recibido
+      3: Procesando
+      4: Respuesta lista
+      */
+
+    }
+    //enviar request
+    xhr.send();
+}
